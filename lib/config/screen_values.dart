74fbd438f21972b0e622a39ee328ccf3147d98c5
babak.lotfalizadeh@geeks.ltd.uk <babak.lotfalizadeh@geeks.ltd.uk>
2022-04-24 18:57:46 +0430

    add static values

---
+++ lib/config/screen_values.dart
@@ -0,0 +1,13 @@
+class ScreenValues {
+  ScreenValues._();
+  static double get borderNormal => 1;
+  static double get paddingNormal => 8;
+  static double get paddingLarge => 16;
+  static double get radiusSmall => 4;
+  static double get radiusNormal => 8;
+  static double get radiusLarge => 16;
+  static double get iconSmall => 20;
+  static double get fontXSmall => 10;
+  static double get fontSmall => 12;
+  static double get progressBarHeight => 20;
+}
---
+++ lib/config/static_values.dart
@@ -0,0 +1,6 @@
+class StaticValues {
+  StaticValues._();
+  static String baseUrl = "https://run.mocky.io/v3";
+  static String randomUrl = "/4e1621cb-d493-4891-ac92-c197c250e1ae";
+  static int successCode = 200;
+}
